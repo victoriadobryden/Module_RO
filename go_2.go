0dd15d65e762acc2cc53c4f21debf7cbe0bc324d
Victoria Goodday <65075884+victoriadobryden@users.noreply.github.com>
2021-11-04 15:15:52 +0200

    Update go_2.go

--- go_2.go
+++ go_2.go
@@ -1,43 +1,85 @@
 package main
 
 import (
-  "fmt"
-  "math/rand"
-  "strconv"
-  "sync"
-  "time"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
-type Airplane struct {
-  name       string
-  trapCounts int
+var NumberOfServices = 15
+
+type Book struct {
+	name string
+	author string
 }
 
-func (airplane Airplane) goToRandomPlace(wg *sync.WaitGroup) {
-  distances := []int{1, 2, 3, 1, 4}
-  places := []string{"A", "B", "B", "D", "F"}
-  place := rand.Intn(len(distances))
-  fmt.Println(airplane.name, "went to", places[place])
-  <-time.After(time.Second * time.Duration(distances[place]))
-  fmt.Println(airplane.name, "arrived to", places[place])
-  defer wg.Done()
+type Reader struct {
+	name string
 }
 
-func arrive(wg *sync.WaitGroup, i int, after int) {
-  <-time.After(time.Second * time.Duration(after))
-  airplane := Airplane{strconv.FormatInt(int64(i), 10), 2}
-  fmt.Println("Airplane", airplane.name, "arrive")
-  wg.Add(1)
-  go airplane.goToRandomPlace(wg)
-  defer wg.Done()
+type Librarian struct { }
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func (reader Reader) takeBook(booksChan chan Book, readerSemaphore chan int,
+	librarianSemaphore chan bool) {
+	for {
+		if <-readerSemaphore == -1 {
+			return
+		}
+		firstBook := <-booksChan
+		secondBook := <-booksChan
+		fmt.Println("Reader "+reader.name+" got books:",
+			firstBook, ", ", secondBook)
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println("Reader " + reader.name + " read books.")
+		fmt.Println()
+		librarianSemaphore <- true
+	}
+}
+
+func (Librarian) giveBooks(partsChan chan Book, readerSemaphore chan int,
+	librarianSemaphore chan bool, endChan chan bool) {
+	for i := 0; i < NumberOfServices; i++ {
+		if i != 0 {
+			<-librarianSemaphore
+		}
+		var firstBook = Book{name: randSeq(10), author: randSeq(8)}
+		var secondBook = Book{name: randSeq(10), author: randSeq(8)}
+		fmt.Println("Librarian sent ", firstBook, " and ",
+			secondBook)
+		partsChan <- firstBook
+		partsChan <- secondBook
+		readerSemaphore <- 1
+	}
+	<-librarianSemaphore
+	readerSemaphore <- -1
+	readerSemaphore <- -1
+	readerSemaphore <- -1
+	endChan <- true
 }
 
 func main() {
-  var wg sync.WaitGroup
-  for i := 0; i < 10; i++ {
-    wg.Add(1)
-    go arrive(&wg, i, rand.Intn(i+1))
-  }
+	reader1 := Reader{name: "Alex"}
+	reader2 := Reader{name: "Dasha"}
+	reader3 := Reader{name: "Andrew"}
 
-  wg.Wait()
+	booksChan := make(chan Book, 2)
+	librarianSemaphore := make(chan bool, 1)
+	readerSemaphore := make(chan int, 1)
+	endChan := make(chan bool, 1)
+
+	go Librarian{}.giveBooks(booksChan, readerSemaphore, librarianSemaphore, endChan)
+	go reader1.takeBook(booksChan, readerSemaphore, librarianSemaphore)
+	go reader2.takeBook(booksChan, readerSemaphore, librarianSemaphore)
+	go reader3.takeBook(booksChan, readerSemaphore, librarianSemaphore)
+	<-endChan
 }
