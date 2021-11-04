da46cb49add4ca2fa4286c2af08b67873d6afaa1
Victoria Goodday <65075884+victoriadobryden@users.noreply.github.com>
2021-11-04 14:34:11 +0200

    Update go_2.go

--- go_2.go
+++ go_2.go
@@ -1,24 +1,43 @@
 package main
 
 import (
-	"fmt"
-	"time"
-	"sync"
+  "fmt"
+  "math/rand"
+  "strconv"
+  "sync"
+  "time"
 )
 
-func main() {
-	var wg sync.WaitGroup
-	start := time.Now()
-	for i := 1; i <= 10; i++ {
-		wg.Add(1)
-		go executeTask(i, &wg)
-	}
-	wg.Wait()
-	fmt.Println("Total execution time:", time.Since(start))
+type Airplane struct {
+  name       string
+  trapCounts int
 }
 
-func executeTask(i int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Println("Executing task", i)
-	time.Sleep(250 * time.Millisecond)
+func (airplane Airplane) goToRandomPlace(wg *sync.WaitGroup) {
+  distances := []int{1, 2, 3, 1, 4}
+  places := []string{"A", "B", "B", "D", "F"}
+  place := rand.Intn(len(distances))
+  fmt.Println(airplane.name, "went to", places[place])
+  <-time.After(time.Second * time.Duration(distances[place]))
+  fmt.Println(airplane.name, "arrived to", places[place])
+  defer wg.Done()
+}
+
+func arrive(wg *sync.WaitGroup, i int, after int) {
+  <-time.After(time.Second * time.Duration(after))
+  airplane := Airplane{strconv.FormatInt(int64(i), 10), 2}
+  fmt.Println("Airplane", airplane.name, "arrive")
+  wg.Add(1)
+  go airplane.goToRandomPlace(wg)
+  defer wg.Done()
+}
+
+func main() {
+  var wg sync.WaitGroup
+  for i := 0; i < 10; i++ {
+    wg.Add(1)
+    go arrive(&wg, i, rand.Intn(i+1))
+  }
+
+  wg.Wait()
 }
